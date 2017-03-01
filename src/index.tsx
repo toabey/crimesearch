1a16c61b1602aa4dfe2f426fed3a31bf64e43dda
abey <toabey@gmail.com>
2017-03-01 17:15:45 +0530

    starting functionality

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
 
-
+import {OpeneGovPractices} from "./app/crimesearch";
 
 import {Router, Route, IndexRoute} from "react-router";
 import  createBrowserHistory from 'history/lib/createBrowserHistory'
@@ -10,7 +10,7 @@ import  createBrowserHistory from 'history/lib/createBrowserHistory'
 ReactDOM.render((
     <Router history={createBrowserHistory()}>
 
-
+        <Route component={OpeneGovPractices} path="/"/>
 
     </Router>
 ), document.getElementById('root'));
